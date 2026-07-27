ea25653719a150778e0bbf30b16c041b11ab0ad1
still-spidy <devianks25@gmail.com>
2026-07-27 16:00:53 -0400

    Extract server banner from HTTP response

--- grabber.py
+++ grabber.py
@@ -3,21 +3,26 @@ import socket
 hostname = input("Enter target: ")
 ip = socket.gethostbyname(hostname)
 port = 80
+print(f"Connecting to {hostname}....")
 data = ("GET / HTTP/1.1\r\n"
 f"Host: {hostname}\r\n"
 "Connection: close\r\n"
 "\r\n")
-print(repr(data))
-print(data.encode())
-
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.settimeout(10)
 status = s.connect_ex((ip,port))
-print(status)
-print(s.sendall(data.encode()))
+if status==0:
+    print("Connection Successfull!!")
+else:
+    print("Connection Failed!!")
 s.sendall(data.encode())
 result = s.recv(1024)
 s.close()
 result_decode = result.decode()
-print(result_decode)
+
+for line in result_decode.splitlines():
+    if line.lower().startswith("server:"):
+        print(f"Banner is {line}")
+
+print("Program Completed!!")
