30a9fc6bd7299d711bf01512b2c58c4320321134
still-spidy <devianks25@gmail.com>
2026-07-27 16:12:12 -0400

    Add connection handling and some error handling

--- grabber.py
+++ grabber.py
@@ -1,7 +1,11 @@
 import socket
 
-hostname = input("Enter target: ")
-ip = socket.gethostbyname(hostname)
+try: 
+    hostname = input("Enter target: ")
+    ip = socket.gethostbyname(hostname)
+except:
+    print("Invalid Hostname or IP Address")
+    exit()
 port = 80
 print(f"Connecting to {hostname}....")
 data = ("GET / HTTP/1.1\r\n"
@@ -12,17 +16,26 @@ f"Host: {hostname}\r\n"
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.settimeout(10)
 status = s.connect_ex((ip,port))
-if status==0:
-    print("Connection Successfull!!")
+if status!=0:
+    print("Connection Failed! \n Exiting Program.....bye")
+    exit()
 else:
-    print("Connection Failed!!")
+    print("Connection Successfull!!")
 s.sendall(data.encode())
 result = s.recv(1024)
 s.close()
 result_decode = result.decode()
 
+banner_found=False
 for line in result_decode.splitlines():
     if line.lower().startswith("server:"):
-        print(f"Banner is {line}")
+        print(f"Banner Found:\n{line}")
+        banner_found=True
+
+if not banner_found:
+    print("No banner Found.")
+    response = input("Want to see server response(Y/N)?")
+    if response.lower()=="y":
+        print(result_decode) 
 
 print("Program Completed!!")
